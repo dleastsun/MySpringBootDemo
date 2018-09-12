a0ca822b0fea481b87d5b11c5f5cd562dc823416
lixudong <lixudong@LAPTOP-VONPL8NO>
2018-09-12 21:45:40 +0800

    beanutils

--- src/main/java/com/eai/mybatis/domain/UserInfo.java
+++ src/main/java/com/eai/mybatis/domain/UserInfo.java
@@ -112,4 +112,11 @@ public class UserInfo implements Serializable {
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
+	@Override
+	public String toString() {
+		return "UserInfo [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+	
+	
 }
