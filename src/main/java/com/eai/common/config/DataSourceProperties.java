58cb5fa167f3f6f91b8cbbf201e4aa511645abde
lixudong <lixudong@LAPTOP-VONPL8NO>
2018-09-15 10:33:03 +0800

    1

--- src/main/java/com/eai/common/config/DataSourceProperties.java
+++ src/main/java/com/eai/common/config/DataSourceProperties.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix=DataSourceProperties.DS)
 
 public class DataSourceProperties {
+	
 	public  static final String DS="";
 	
 	private String userName;
