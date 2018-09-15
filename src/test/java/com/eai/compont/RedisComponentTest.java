7763f0a58a3e248c1d54eccd1bfb9a8d90ea6bc9
lixudong <lixudong@LAPTOP-VONPL8NO>
2018-09-15 23:52:36 +0800

    a

---
+++ src/test/java/com/eai/compont/RedisComponentTest.java
@@ -0,0 +1,31 @@
+//package com.eai.compont;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class RedisComponentTest {
+//
+//	@Autowired
+//	private RedisComponent redisComponent;
+//
+//	@Test
+//	public void set() {
+//		redisComponent.set("roncoo", "hello world");
+//	}
+//
+//	@Test
+//	public void get() {
+//		System.out.println(redisComponent.get("roncoo"));
+//	}
+//	
+//	@Test
+//	public void del() {
+//		redisComponent.del("roncoo");
+//	}
+//
+//}
