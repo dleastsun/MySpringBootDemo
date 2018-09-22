e9ffd2fe02239d966b11f5d978378f20af008748
lixudong <lixudong@LAPTOP-VONPL8NO>
2018-09-22 11:09:02 +0800

    a

--- src/main/java/com/eai/mybatis/dao/UserDao.java
+++ src/main/java/com/eai/mybatis/dao/UserDao.java
@@ -2,8 +2,13 @@ package com.eai.mybatis.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 import com.eai.springbatch.model.User;
 
+@Mapper
+@Repository
 public interface UserDao {
 	
 	public List<User> select(User user);
