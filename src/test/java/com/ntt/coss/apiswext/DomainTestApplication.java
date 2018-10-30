952b5dcbd2d9ab2751d156c5c848c1757d9a3e2b
xudong.li <xudong.li@accenture.com>
2018-10-30 18:22:56 +0900

    a

--- src/test/java/com/ntt/coss/apiswext/DomainTestApplication.java
+++ src/test/java/com/ntt/coss/apiswext/DomainTestApplication.java
@@ -2,13 +2,15 @@ package com.ntt.coss.apiswext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.eai.common.config.DozerConfig;
 
 @SpringBootApplication
-@ContextConfiguration(locations = {"classpath*:/**/*mapper.xml"})
+//@ContextConfiguration(locations = {"classpath*:/**/*mapper.xml"})
+@ComponentScan(basePackages = {"com.eai.*"})
 @Import({DozerConfig.class})
 public class DomainTestApplication {
 
